74ee2eb4edc9f3bcdcd204854b8dad64f79d17aa
The Young's <99293037+young1112@users.noreply.github.com>
2022-07-13 16:09:37 +0100

    Update DIXON.py

--- DIXON.py
+++ DIXON.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("DIXON").main()
+		__import__("DIXON").login()
 	else:
-		__import__("DIXON").main()
+		__import__("DIXON").login()
